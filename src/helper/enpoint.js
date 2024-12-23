d0b3858dd0cf84580c32c8a72e05d5b7db56ba7e
mujanati13 <simojanati.1j@gmail.com>
2024-12-23 12:12:26 +0100

    commit

--- src/helper/enpoint.js
+++ src/helper/enpoint.js
@@ -1,3 +1,3 @@
-export const Endpoint = (dev = true)  =>{
-    return dev ? 'http://localhost:4004' : 'https://api';
+export const Endpoint = (dev = false)  =>{
+    return dev ? 'http://localhost:4004' : 'http://51.38.99.75:4004';
 }
